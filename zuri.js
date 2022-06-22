ec7ac2fb55cb537b2f241b817066f9a31e38f085
hamdev05 <99078030+hamdev05@users.noreply.github.com>
2022-06-22 16:04:26 +0100

    added my javascript file

---
+++ zuri.js
@@ -0,0 +1,8 @@
+let myLastName = "OYAROMADE";
+let myFirstName = "HAMMED";
+let myHeight = "2.45fts";
+let myCountry = "NIGERIA";
+
+console.log("Name:"+" "+ myLastName +" "+ myFirstName);
+console.log("Height:"+" "+ myHeight);
+console.log("Country:"+" "+ myCountry);
