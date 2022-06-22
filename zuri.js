6acec440914b83d19f7c24ae994affe9067f7a88
hamdev05 <99078030+hamdev05@users.noreply.github.com>
2022-06-22 17:53:54 +0100

    Delete zuri.js

--- zuri.js
+++
@@ -1,8 +0,0 @@
-let myLastName = "OYAROMADE";
-let myFirstName = "HAMMED";
-let myHeight = "2.45fts";
-let myCountry = "NIGERIA";
-
-console.log("Name:"+" "+ myLastName +" "+ myFirstName);
-console.log("Height:"+" "+ myHeight);
-console.log("Country:"+" "+ myCountry);
